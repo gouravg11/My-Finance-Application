b737eedfd8e3de81c7f9b1e24e4f3f18083bb081
gouravg11 <ggupta1289@gmail.com>
2025-03-01 16:39:45 +0530

    Dashboard page done except piechar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/auth/sign-in";
 import SignUp from "./pages/auth/sign-up";
@@ -9,6 +9,7 @@ import Transactions from "./pages/transactions";
 import useStore from "./store";
 import { setAuthToken } from "./libs/apiCall";
 import { Toaster } from "sonner";
+import Navbar from "./components/ui/navbar";
 
 const RootLayout = () => {
   const { user } = useStore((state) => state);
@@ -19,7 +20,7 @@ const RootLayout = () => {
     <Navigate to="sign-in" replace={true} />
   ) : (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="min-h-[cal(h-screen - 100px)]">
         <Outlet />
       </div>
@@ -30,10 +31,18 @@ const RootLayout = () => {
 console.log("I am in app");
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { theme } = useStore((state) => state);
 
+  useEffect(() => {
+    console.log("in useEffect ", theme);
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
   return (
-    <>
+    <main>
       <div className="w-full min-h-screen px-6 bg-gray-100 md:px-20 dark:bg-slate-900">
         <Routes>
           <Route element={<RootLayout />}>
@@ -48,7 +57,7 @@ function App() {
         </Routes>
       </div>
       <Toaster richColors position="top-center " />
-    </>
+    </main>
   );
 }
 
