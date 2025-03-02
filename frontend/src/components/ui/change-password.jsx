1ba13880154e2ad6a3d9e4c03d451a4a09d155c9
gouravg11 <ggupta1289@gmail.com>
2025-03-02 14:43:08 +0530

    setting page done except country part

---
+++ frontend/src/components/ui/change-password.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { toast } from "sonner";
+import Input from "./input";
+import { useForm } from "react-hook-form";
+import { Button } from "./button";
+import { BiLoader } from "react-icons/bi";
+import api from "../../libs/apiCall";
+
+const ChangePassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  const [loading, setLoading] = useState(false);
+  const submitPasswordHandler = async (data) => {
+    try {
+      setLoading(true);
+
+      const { data: res } = await api.put(`/user/change-password`, data);
+
+      if (res?.status === "success") {
+        toast.success(res?.success);
+      }
+    } catch (error) {
+      console.error("Something went wrong :", error);
+      toast.error(error?.response?.data?.message || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="py-20">
+      <form onSubmit={handleSubmit(submitPasswordHandler)}>
+        <div className="">
+          <p className="text-xl font-bold text-black dark:text-white mb-1">
+            Change Password
+          </p>
+          <span className="labelStyles">
+            This will be used to log into your account and complete high
+            severity actions.
+          </span>
+
+          <div className="mt-6">
+            <Input
+              disabled={loading}
+              type="password"
+              className="inputStyle"
+              name="currentPassword"
+              label="Current Password"
+              {...register("currentPassword", {
+                required: "Current Password is required!",
+              })}
+              error={
+                errors.currentPassword ? errors.currentPassword.message : ""
+              }
+            />
+            <Input
+              disable={loading}
+              type="password"
+              name="newPassword"
+              className="inputStyle"
+              label="New Password"
+              {...register("newPassword", {
+                required: "New Password is required!",
+              })}
+              error={errors.newPassword ? errors.newPassword.message : ""}
+            />
+
+            <Input
+              disable={loading}
+              type="password"
+              name="newPassword"
+              className="inputStyle"
+              label="Confirm Password"
+              {...register("confirmPassword", {
+                required: "Confirm Password is required!",
+                validate: (val) => {
+                  const { newPassword } = getValues();
+                  return newPassword === val || "Password does't match!";
+                },
+              })}
+              error={
+                errors.confirmPassword ? errors.confirmPassword.message : ""
+              }
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-6 justify-end my-5 pb-10 border-b-2 border-gray-200 dark:border-gray-800">
+          <Button
+            variant="outline"
+            loading={loading}
+            type="reset"
+            className="px-6 bg-transparent text-black dark:text-white border bordre-gray-200 dark:border-gray-800"
+          >
+            Reset
+          </Button>
+
+          <Button
+            loading={loading}
+            type="submit"
+            className="px-8 bg-violet-800 text-white"
+          >
+            {loading ? (
+              <BiLoader className="animate-spin text-white" />
+            ) : (
+              "Change Password"
+            )}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ChangePassword;
--- frontend/src/pages/settings.jsx
+++ frontend/src/pages/settings.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import Title from "../components/ui/title";
+import useStore from "../store";
+import SettingsForm from "../components/ui/settingsForm";
+import ChangePassword from "../components/ui/change-password";
+
 const Settings = () => {
-  return <div></div>;
+  const { user } = useStore((state) => state);
+  return (
+    <>
+      <div className="flex flex-col items-center w-full">
+        <div className="w-full max-w-4xl px-4 py-4 my-6 shadow-lg bg-gray-50 dark:bg-black/20 md:px-10 md:py-10">
+          <div className="mt-6 border-b-2 border-gray-200 dark:border-gray-800">
+            <Title title="General Settings" />
+          </div>
+          <div className="py-10">
+            <p className="text-lg font-bold text-black dark:text-white">
+              Profile Infomation
+            </p>
+            <div className="flex items-center gap-4 my-8">
+              <div className="flex items-center justify-center w-12 h-12 font-bold text-2xl text-white rounder-full cursor-pointer bg-violet-600">
+                <p>{user?.firstname.charAt(0)}</p>
+              </div>
+              <p className="text-2xl font-semibold text-black dark:text-gray-400">
+                {user?.firstname}
+              </p>
+            </div>
+            <SettingsForm />
+
+            {!user?.provided && <ChangePassword />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Settings;
